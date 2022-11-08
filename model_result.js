8f6060bf1f5ba42a323e8e67c12157185fa4b964
KangYoungSeo <22019112024@dgu.ac.kr>
2022-11-08 14:04:30 +0900

    add

--- model_result.js
+++ model_result.js
@@ -7,19 +7,10 @@ var spawn = require('child_process').spawn;
 // main function
 async function main() {
 
-    //load classification model
-    //await tf.enableProdMode();
-    //await tf.setBackend('tensorflow');
-    //await tf.ENV.set('DEBUG', false);
-    //await tf.ready();
-    //model =  await tf.loadModel(modelpath);
-
     //https://indesire.tistory.com/159
     //python 로드
 
-    var process = spawn('python3', [
-        FILE_ROOT_DIR + './model_classification.py',
-    ]);
+    var process = spawn('python', [FILE_ROOT_DIR + './model_test.py']);
     // python3라는 명령어로 두번째 인자값에 있는 파일을 실행하겠다는 겁니다.
     // python 파일에서 print를 하면 그 print된 text가 chunk에 담겨져 나오게 됩니다. 
     // 이걸 이용, 가공해서 쓰시면 되겠습니다. node.js에서 프로세스로 실행시킨
@@ -27,12 +18,14 @@ async function main() {
     // 해당 크롤링 데이터 JSON형태로 출력시키게 해놓았습니다.
     // 만약 해당 routing 경로로 호출하게 된다면
     
-
     let argument = [[160, 145, 176, 164, 112, 72, 176, 176]];
 
     // import python code
-    const pythonClassification = spawn('python3', ['-c', `import model; model.pose_classification(${argument});`]);
+    // pose_classification(${argument}
+    const pythonClassification  
+        = spawn('python', ['-c', `import model_test.py; model_test.main());`]);
 
+    
     pythonClassification.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
     });
@@ -40,8 +33,6 @@ async function main() {
     pythonClassification.on('exit', (code) => {
         console.log(`Classification process ended with code`);
     });
-
-    //const res = model.execute(img.tensor);
 
     /*
     $.ajax({
@@ -54,3 +45,5 @@ async function main() {
     */
     
 }
+
+
---
+++ model_test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# interpreter  python 3.7.2
+
+def pose_classification (list) : 
+    print(list)
+    result = 'harry'
+    return result
+
+def main () :
+    print("start")
+
+
+if __name__ == "__main__" : 
+    main()
+    pose_classification([[160, 145, 176, 164, 112, 72, 176, 176]])
