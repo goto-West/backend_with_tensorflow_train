075534026b8765b559b6c62351550602fcf16a9f
KangYoungSeo <22019112024@dgu.ac.kr>
2022-11-11 05:21:10 +0900

    js 내 python 명령형 argument 자료형 변환

--- model_result.js
+++ model_result.js
@@ -7,14 +7,17 @@ var spawn = require('child_process').spawn;
 // main function
 async function main() {
 
-    let argument = [[160, 145, 176, 164, 112, 72, 176, 176]];
+    let argument = [160, 145, 176, 164, 112, 72, 176, 176];
 
     console.log("workings in js");
     //load python 
-    const pythonClassification = spawn('python', ['model_test.py',argument]);
+    const pythonClassification 
+        = spawn('python', ['model.py', 
+        argument[0], argument[1], argument[2], argument[3],
+        argument[4], argument[5], argument[6], argument[7]]);
     
     //pid number
-    console.log(pythonClassification.pid.toString());
+    console.log("pid : " + pythonClassification.pid.toString());
 
     pythonClassification.stdout.on('data', function(data) {
         console.log(data.toString());
