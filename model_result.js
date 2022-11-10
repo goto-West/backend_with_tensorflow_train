87db8122d6d16985aa75811ca6518f01c398a1b4
KangYoungSeo <22019112024@dgu.ac.kr>
2022-11-10 22:22:19 +0900

    rewrite js files

--- model_result.js
+++ model_result.js
@@ -1,39 +1,42 @@
-const fs = require('fs');
-const path = require('path');
-const process = require('process');
+//const fs = require('fs');
+//const path = require('path');
+//const process = require('process');
 
 var spawn = require('child_process').spawn;
 
 // main function
 async function main() {
 
-    //https://indesire.tistory.com/159
-    //python 로드
-
-    var process = spawn('python', [FILE_ROOT_DIR + './model_test.py']);
-    // python3라는 명령어로 두번째 인자값에 있는 파일을 실행하겠다는 겁니다.
-    // python 파일에서 print를 하면 그 print된 text가 chunk에 담겨져 나오게 됩니다. 
-    // 이걸 이용, 가공해서 쓰시면 되겠습니다. node.js에서 프로세스로 실행시킨
-    // 이 파이썬 파일은 네이버 실시간 검색어를 크롤링하는 파이썬 파일입니다.
-    // 해당 크롤링 데이터 JSON형태로 출력시키게 해놓았습니다.
-    // 만약 해당 routing 경로로 호출하게 된다면
-    
     let argument = [[160, 145, 176, 164, 112, 72, 176, 176]];
 
-    // import python code
-    // pose_classification(${argument}
-    const pythonClassification  
-        = spawn('python', ['-c', `import model_test.py; model_test.main());`]);
-
+    console.log("workings in js");
+    //load python 
+    const pythonClassification = spawn('python', ['model_test.py',argument]);
     
+    //pid number
+    console.log(pythonClassification.pid.toString());
+
+    pythonClassification.stdout.on('data', function(data) {
+        console.log(data.toString());
+    })  
+    
+    pythonClassification.stderr.on('data', function(data) {
+        console.log(data.toString());
+        
+    })  
+    
+    /*
     pythonClassification.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
     });
 
-    pythonClassification.on('exit', (code) => {
+    pythonClassification.on('data', (data) => {
+        console.log(`error:${data}`);
+    });
+    
+    pythonClassification.on('exit', () => {
         console.log(`Classification process ended with code`);
     });
-
     /*
     $.ajax({
         type: "POST",
@@ -46,4 +49,5 @@ async function main() {
     
 }
 
+main();
 
--- model_test.py
+++ model_test.py
@@ -5,14 +5,16 @@ import matplotlib.pyplot as plt
 # interpreter  python 3.7.2
 
 def pose_classification (list) : 
-    print(list)
+    # print(list)
     result = 'harry'
     return result
 
-def main () :
-    print("start")
+def main (argv) :
+    result = pose_classification(argv)
+    print("result : "+ result)
+    return result
 
 
 if __name__ == "__main__" : 
     main()
-    pose_classification([[160, 145, 176, 164, 112, 72, 176, 176]])
+    
